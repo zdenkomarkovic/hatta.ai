2df1688fff0e565f8946bbbdddb705dee9df45e7
Zdenko Markovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-08-20 19:30:05 +0200

    Dodaj Toaster i popravi sajt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import ButtonToTop from "@/components/ButtonToTop";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -42,6 +43,7 @@ export default function RootLayout({
         {children}
         <ButtonToTop />
         <Footer />
+        <Toaster />
       </body>
     </html>
   );
