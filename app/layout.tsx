c58dbb69e9172871df8fb1c939491455211d5bc6
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-08-25 02:33:23 +0200

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Playfair_Display } from "next/font/google";
+import { Inter} from "next/font/google";
 import "./globals.css";
 import ParticlesBackground from "@/components/ParticlesBackground";
 
